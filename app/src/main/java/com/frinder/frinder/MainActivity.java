da74df7d9754815d5a1d5774732c5d95c2a54439
Mallika Viswas <mallikaviswas@users.noreply.github.com>
2017-10-08 14:05:01 -0700

    Setup crash reporting through Fabric

--- app/src/main/java/com/frinder/frinder/MainActivity.java
+++ app/src/main/java/com/frinder/frinder/MainActivity.java
@@ -1,13 +1,38 @@
 package com.frinder.frinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fabric.with(this, new Crashlytics());
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
         setContentView(R.layout.activity_main);
     }
+
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
+    }
+
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        //Crashlytics.setUserIdentifier("12345");
+        //Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("Mallika Viswas");
+    }
+
 }
