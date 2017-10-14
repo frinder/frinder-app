0c3b0fb32231c8a3ba6e428634db83a78cbd80a2
Dony George <donygeorge@gmail.com>
2017-10-14 14:40:02 -0700

    [Requests] Add tab layout

---
+++ app/src/main/java/com/frinder/frinder/adapters/RequestsFragmentPagerAdapter.java
@@ -0,0 +1,45 @@
+package com.frinder.frinder.adapters;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.frinder.frinder.R;
+import com.frinder.frinder.fragments.NoRequestsFragment;
+
+public class RequestsFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private String mTabTitles[];
+    private Context mContext;
+
+    public RequestsFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+        Resources resources = context.getResources();
+        mTabTitles = new String[] {
+                resources.getString(R.string.title_incoming),
+                resources.getString(R.string.title_outgoing),
+                resources.getString(R.string.title_accepted)
+        };
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // TODO: Change this
+        return NoRequestsFragment.newInstance();
+    }
+
+    @Override
+    public int getCount() {
+        return mTabTitles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return mTabTitles[position];
+    }
+}
