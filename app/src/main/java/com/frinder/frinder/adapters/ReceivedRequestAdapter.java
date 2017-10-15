b28f3f8dd9e9d1a3cf263bef975653b3295e567b
Dony George <donygeorge@gmail.com>
2017-10-15 00:36:33 -0700

    Add buttons to respond to requests

--- app/src/main/java/com/frinder/frinder/adapters/ReceivedRequestAdapter.java
+++ app/src/main/java/com/frinder/frinder/adapters/ReceivedRequestAdapter.java
@@ -1,10 +1,18 @@
 package com.frinder.frinder.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.frinder.frinder.R;
 import com.frinder.frinder.model.Request;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReceivedRequestAdapter extends RequestsAdapter {
 
@@ -14,6 +22,32 @@ public class ReceivedRequestAdapter extends RequestsAdapter {
 
     String getUserId(Request request) {
         return request.senderId;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View requestView = inflater.inflate(R.layout.item_request_received, parent, false);
+
+        // Return a new holder instance
+        ReceivedViewHolder viewHolder = new ReceivedViewHolder(requestView);
+        return viewHolder;
+    }
+
+    public class ReceivedViewHolder extends RequestsAdapter.ViewHolder {
+
+        @BindView(R.id.tvAccept)
+        TextView tvAccept;
+        @BindView(R.id.tvDeny)
+        TextView tvDeny;
+
+        public ReceivedViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 
 }
